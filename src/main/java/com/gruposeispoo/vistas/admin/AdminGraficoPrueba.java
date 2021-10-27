d2a3dc67922cd3d7d3bfd3eda85e84cd04b7397e
facumoransi <moranfcnd@gmail.com>
2021-10-27 16:40:08 -0300

    Fixing push errors

---
+++ src/main/java/com/gruposeispoo/vistas/admin/AdminGraficoPrueba.java
@@ -0,0 +1,286 @@
+package com.gruposeispoo.vistas.admin;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class AdminGraficoPrueba extends javax.swing.JPanel {
+
+    public AdminGraficoPrueba() {
+        initComponents();
+        crearGraficoBarras("Pregunta 1");
+        crearGraficoBarras("Pregunta 2");
+    }
+
+    private CategoryDataset crearDataset() {
+        final String escala1 = "Totalmente desacuerdo";
+        final String escala2 = "Desacuerdo";
+        final String escala3 = "Neutro";
+        final String escala4 = "De acuerdo";
+        final String escala5 = "Totalmente de acuerdo";
+        
+        final String pregunta = "Respuestas";
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        //Escala 1 = 1 sola persona respondio
+        dataset.addValue(1.0, escala1, pregunta);
+        dataset.addValue(0.0, escala2, pregunta);
+        dataset.addValue(2.0, escala3, pregunta);
+        dataset.addValue(4.0, escala4, pregunta);
+        dataset.addValue(4.0, escala5, pregunta);
+        
+        return dataset;
+    }
+
+    private void crearGraficoBarras(String nombre) {
+        JFreeChart barras = ChartFactory.createBarChart(nombre, "Personas", "Escala", crearDataset(), PlotOrientation.VERTICAL, true, true, false);
+        ChartPanel panel = new ChartPanel(barras);
+        panel.setPreferredSize(new Dimension(600,370));
+        contentContainer.add(panel);
+    }
+
+    //candidato.setAlignmentX(Component.CENTER_ALIGNMENT);
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bgContainer = new javax.swing.JPanel();
+        headerContainer = new javax.swing.JPanel();
+        headerTitleTxt = new javax.swing.JLabel();
+        headerBarContainer = new javax.swing.JPanel();
+        btnExitContainer = new javax.swing.JPanel();
+        btnExitTxt = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        contentContainer = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        btnVotarContainer = new javax.swing.JPanel();
+        btnVotarTxt = new javax.swing.JLabel();
+
+        bgContainer.setBackground(new java.awt.Color(245, 244, 246));
+
+        headerContainer.setBackground(new java.awt.Color(245, 244, 246));
+
+        headerTitleTxt.setBackground(new java.awt.Color(245, 244, 246));
+        headerTitleTxt.setFont(new java.awt.Font("Roboto Black", 1, 40)); // NOI18N
+        headerTitleTxt.setForeground(new java.awt.Color(43, 179, 205));
+        headerTitleTxt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        headerTitleTxt.setText("Sistema auditor de votos");
+
+        headerBarContainer.setBackground(new java.awt.Color(245, 244, 246));
+        headerBarContainer.setForeground(new java.awt.Color(255, 255, 255));
+        headerBarContainer.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                headerBarContainerMouseDragged(evt);
+            }
+        });
+        headerBarContainer.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                headerBarContainerMousePressed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout headerBarContainerLayout = new javax.swing.GroupLayout(headerBarContainer);
+        headerBarContainer.setLayout(headerBarContainerLayout);
+        headerBarContainerLayout.setHorizontalGroup(
+            headerBarContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        headerBarContainerLayout.setVerticalGroup(
+            headerBarContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 31, Short.MAX_VALUE)
+        );
+
+        btnExitContainer.setBackground(new java.awt.Color(255, 255, 255));
+        btnExitContainer.setForeground(new java.awt.Color(255, 255, 255));
+
+        btnExitTxt.setBackground(new java.awt.Color(199, 80, 80));
+        btnExitTxt.setFont(new java.awt.Font("Roboto Black", 0, 25)); // NOI18N
+        btnExitTxt.setForeground(new java.awt.Color(245, 244, 246));
+        btnExitTxt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btnExitTxt.setText("x");
+        btnExitTxt.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnExitTxt.setOpaque(true);
+        btnExitTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnExitTxtMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnExitTxtMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnExitTxtMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout btnExitContainerLayout = new javax.swing.GroupLayout(btnExitContainer);
+        btnExitContainer.setLayout(btnExitContainerLayout);
+        btnExitContainerLayout.setHorizontalGroup(
+            btnExitContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btnExitContainerLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnExitTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        btnExitContainerLayout.setVerticalGroup(
+            btnExitContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(btnExitTxt, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout headerContainerLayout = new javax.swing.GroupLayout(headerContainer);
+        headerContainer.setLayout(headerContainerLayout);
+        headerContainerLayout.setHorizontalGroup(
+            headerContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(headerContainerLayout.createSequentialGroup()
+                .addContainerGap(60, Short.MAX_VALUE)
+                .addComponent(headerTitleTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 581, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(59, 59, 59))
+            .addGroup(headerContainerLayout.createSequentialGroup()
+                .addComponent(headerBarContainer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(24, 24, 24)
+                .addComponent(btnExitContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        headerContainerLayout.setVerticalGroup(
+            headerContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, headerContainerLayout.createSequentialGroup()
+                .addGroup(headerContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(headerBarContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnExitContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(headerTitleTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        contentContainer.setBackground(new java.awt.Color(245, 244, 246));
+        contentContainer.setLayout(new javax.swing.BoxLayout(contentContainer, javax.swing.BoxLayout.Y_AXIS));
+        jScrollPane1.setViewportView(contentContainer);
+
+        jPanel1.setBackground(new java.awt.Color(245, 244, 246));
+
+        btnVotarTxt.setBackground(new java.awt.Color(43, 179, 205));
+        btnVotarTxt.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
+        btnVotarTxt.setForeground(new java.awt.Color(255, 255, 255));
+        btnVotarTxt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btnVotarTxt.setText("VOTAR");
+        btnVotarTxt.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnVotarTxt.setOpaque(true);
+        btnVotarTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnVotarTxtMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnVotarTxtMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout btnVotarContainerLayout = new javax.swing.GroupLayout(btnVotarContainer);
+        btnVotarContainer.setLayout(btnVotarContainerLayout);
+        btnVotarContainerLayout.setHorizontalGroup(
+            btnVotarContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(btnVotarTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
+        );
+        btnVotarContainerLayout.setVerticalGroup(
+            btnVotarContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btnVotarContainerLayout.createSequentialGroup()
+                .addComponent(btnVotarTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnVotarContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnVotarContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout bgContainerLayout = new javax.swing.GroupLayout(bgContainer);
+        bgContainer.setLayout(bgContainerLayout);
+        bgContainerLayout.setHorizontalGroup(
+            bgContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgContainerLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(bgContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1)
+                    .addComponent(headerContainer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+        );
+        bgContainerLayout.setVerticalGroup(
+            bgContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgContainerLayout.createSequentialGroup()
+                .addComponent(headerContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bgContainer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bgContainer, javax.swing.GroupLayout.DEFAULT_SIZE, 586, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void headerBarContainerMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_headerBarContainerMouseDragged
+
+    }//GEN-LAST:event_headerBarContainerMouseDragged
+
+    private void headerBarContainerMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_headerBarContainerMousePressed
+
+    }//GEN-LAST:event_headerBarContainerMousePressed
+
+    private void btnExitTxtMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnExitTxtMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_btnExitTxtMouseClicked
+
+    private void btnExitTxtMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnExitTxtMouseEntered
+        this.btnExitTxt.setBackground(Color.red);
+    }//GEN-LAST:event_btnExitTxtMouseEntered
+
+    private void btnExitTxtMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnExitTxtMouseExited
+        this.btnExitTxt.setBackground(new Color(199, 80, 80));
+    }//GEN-LAST:event_btnExitTxtMouseExited
+
+    private void btnVotarTxtMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVotarTxtMouseEntered
+        btnVotarTxt.setBackground(new Color(58, 77, 92));
+    }//GEN-LAST:event_btnVotarTxtMouseEntered
+
+    private void btnVotarTxtMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVotarTxtMouseExited
+        btnVotarTxt.setBackground(new Color(43, 179, 205));
+    }//GEN-LAST:event_btnVotarTxtMouseExited
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bgContainer;
+    private javax.swing.JPanel btnExitContainer;
+    private javax.swing.JLabel btnExitTxt;
+    private javax.swing.JPanel btnVotarContainer;
+    private javax.swing.JLabel btnVotarTxt;
+    private javax.swing.JPanel contentContainer;
+    private javax.swing.JPanel headerBarContainer;
+    private javax.swing.JPanel headerContainer;
+    private javax.swing.JLabel headerTitleTxt;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+
+}
