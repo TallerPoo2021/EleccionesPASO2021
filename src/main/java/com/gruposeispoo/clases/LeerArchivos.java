49becd3bf88c7fd6b5ab9105387e929a57d0700a
Kevin D. Kling <uner.kevin.kling@outlook.es>
2021-10-27 12:06:41 -0300

    lo mismo que el commit anterior, parece que no se comiteo

---
+++ src/main/java/com/gruposeispoo/clases/LeerArchivos.java
@@ -0,0 +1,69 @@
+package com.gruposeispoo.clases;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class LeerArchivos {
+	private File archivo;
+	private FileReader fr;
+	private String[] fila;
+	private CSVReader csvRead;
+
+	/**
+	 * Comprueba que el archivo no este vacio
+	 *
+	 * @param archivo, ruta del archivo a comprobar
+	 * @return boolean, true si esta vacio el archivo
+	 *
+	 */
+	private boolean archivoVacio(File archivo) {
+		return archivo.length() == 0;
+	}
+
+	/**
+	 * Carga de padron
+	 *
+	 * @param rutaArchivoElectores, ruta de Padron.csv
+	 * @return List<Elector>, lista de electores
+	 *
+	 */
+	public List<Elector> cargaElectores(String rutaArchivoElectores) {
+		List<Elector> listaElectores = new ArrayList<Elector>();
+		try {
+			archivo = new File(rutaArchivoElectores);
+			if (this.archivoVacio(archivo)) {
+				throw new Error("El archivo" + rutaArchivoElectores + " esta vacio");
+			}
+
+			fr = new FileReader(archivo);
+			csvRead = new CSVReader(fr);
+			fila = null;
+			while ((fila = csvRead.readNext()) != null) {
+
+				int anioNacimiento = Integer.parseInt(fila[2]);
+				int mesNacimiento = Integer.parseInt(fila[3]);
+				int diaNacimiento = Integer.parseInt(fila[4]);
+				Elector electorAux = new Elector(fila[0], fila[1],
+						LocalDate.of(anioNacimiento, mesNacimiento, diaNacimiento),
+						TipoDocumento.valueOf(fila[5]),
+						new Domicilio(fila[6], fila[7], fila[8], fila[9]),
+						Integer.parseInt(fila[10]));
+
+				listaElectores.add(electorAux);
+			}
+
+			csvRead.close();
+		} catch (IOException | CsvValidationException e) {
+			System.out.println(e);
+		}
+
+		return listaElectores;
+	}
+}
