b9b1de0719c2975a6c4a9262aac49461d29f8785
Gabriel Morales <48184264+GabrielMorales1999@users.noreply.github.com>
2021-10-28 23:13:10 -0300

    Agregado documentacion y corregido un error en Distrito.

--- src/main/java/com/gruposeispoo/clases/Circuito.java
+++ src/main/java/com/gruposeispoo/clases/Circuito.java
@@ -11,6 +11,7 @@ public class Circuito implements IContadorVoto {
     private List<Voto> votosCircuito;
 
     /**
+     * Constructor de Circuito.
      *
      * @param nombre, nombre del circuito.
      * @param numeracion, numero del circuito.
@@ -27,16 +28,24 @@ public class Circuito implements IContadorVoto {
         seccion.agregarCircuito(this);
     }
 
+    /**
+     * Agrega una mesa electoral.
+     * @param mesaElectoral, agrega una mesa electoral.
+     */
     public void agregarMesa(MesaElectoral mesaElectoral){
         listaMesas.add(mesaElectoral);
     }
 
+    /**
+     * Devuelve la seccion a la que pertenece el circuito.
+     * @return la seccion del circuito.
+     */
     public Seccion getSeccion() {
         return seccion;
     }
 
     /**
-     *
+     * Metodo para procesar un voto.
      * @param voto, ingresa la referencia del voto que se creó a la hora que lo emite el Elector.
      * @throws Exception Ocurre cuando la referencia es nula, cuando el elector no debe votar en la mesa o cuando esta persona ya votó.
      */
@@ -48,7 +57,7 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
-     *
+     * Devuelve la lista de votos del circuito.
      * @return Una lista con los votos en el circuito.
      */
     public List<Voto> getVotosCircuito() {
@@ -56,31 +65,47 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
-     *
+     * Devuelve las mesas electorales del circuito.
      * @return Una lista del tipo MesaElectoral, la cuales pertencecen al circuito.
      */
     public List<MesaElectoral> getListaMesas() {
         return listaMesas;
     }
 
+    /**
+     * Devuelve el nombre del circuito.
+     * @return el nombre del circuito.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Cambia el nombre del circuito.
+     * @param nombre, nuevo nombre del circuito.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Devuelve el numero del circuito.
+     * @return numero del circuito.
+     */
     public int getNumeracion() {
         return numeracion;
     }
 
+    /**
+     * Cambia el numero del circuito.
+     * @param numeracion, nuevo numero del circuito.
+     */
     public void setNumeracion(int numeracion) {
         this.numeracion = numeracion;
     }
 
     /**
-     *
+     * Cuenta la cantidad de votos en blanco.
      * @return cantidad de votos en blancos totales.
      */
     @Override
@@ -93,7 +118,7 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
-     *
+     * Cuenta la cantidad de votos en blanco del tipo de candidato que se le pase.
      * @param tipoCandidato, El tipo de candidato que es (senador o diputado).
      * @return devuelve la cantidad de votos en blanco de este tipo de candidato.
      */
@@ -108,7 +133,7 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
-     *
+     * Cuenta los votos de este candidato.
      * @param candidato, referencia de la persona postulada a un cargo.
      * @return cantidad de votos hacia la persona enviada.
      */
@@ -122,7 +147,7 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
-     *
+     * Cuenta los votos de esta lista politica.
      * @param listaPolitica, lista politica de la cual se quiere saber los votos que obtuvo.
      * @return la cantidad de votos que recibió la lista.
      */
@@ -138,6 +163,7 @@ public class Circuito implements IContadorVoto {
     }
 
     /**
+     * Cuenta los votos totales.
      * @return cantidad de votos generales.
      */
     @Override
--- src/main/java/com/gruposeispoo/clases/Seccion.java
+++ src/main/java/com/gruposeispoo/clases/Seccion.java
@@ -11,7 +11,7 @@ public class Seccion implements IContadorVoto {
     private List<Elector> padron;
 
     /**
-     *
+     * Constructor de seccion.
      * @param nombre
      * @param distrito
      * @param padron
@@ -28,36 +28,70 @@ public class Seccion implements IContadorVoto {
         circuitos.add(circuito);
     }
 
-    public List<Voto> getTotalVotos(){
-        return null;
-    }
-
+    /**
+     * Devuelve el nombre de la seccion.
+     * @return nombre de la seccion.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Cambia el nombre de la seccion.
+     * @param nombre, el nuevo nombre de la seccion.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Devuelve los circuitos de la seccion.
+     * @return los circuitos que pertenecen a esta seccion.
+     */
     public List<Circuito> getCircuitos() {
         return circuitos;
     }
 
+    /**
+     * Devuelve el distrito de la seccion.
+     * @return el distrito al que pertenece.
+     */
     public Distrito getDistrito() {
         return distrito;
     }
 
+    /**
+     * Cuenta la cantidad de votos en blanco.
+     * @return cantidad de votos en blancos totales.
+     */
     @Override
     public int contarVotosBlanco() {
-        return 0;
+        int contador = 0;
+        for (Circuito circuito : circuitos) {
+            contador += circuito.contarVotosBlanco();
+        }
+        return contador;
     }
 
+    /**
+     * Cuenta la cantidad de votos en blanco del tipo de candidato que se le pase.
+     * @param tipoCandidato, El tipo de candidato que es (senador o diputado).
+     * @return devuelve la cantidad de votos en blanco de este tipo de candidato.
+     */
     @Override
     public int contarVotosBlanco(TipoCandidato tipoCandidato) {
-        return 0;
+        int contador = 0;
+        for (Circuito circuito : circuitos) {
+            contador += circuito.contarVotosBlanco(tipoCandidato);
+        }
+        return contador;
     }
 
+    /**
+     * Cuenta los votos de este candidato.
+     * @param candidato, referencia de la persona postulada a un cargo.
+     * @return cantidad de votos hacia la persona enviada.
+     */
     @Override
     public int contarVotos(Candidato candidato) {
         int contador = 0;
@@ -67,6 +101,11 @@ public class Seccion implements IContadorVoto {
         return contador;
     }
 
+    /**
+     * Cuenta los votos de esta lista politica.
+     * @param listaPolitica, lista politica de la cual se quiere saber los votos que obtuvo.
+     * @return la cantidad de votos que recibió la lista.
+     */
     @Override
     public int contarVotos(ListaPolitica listaPolitica) {
         int contador = 0;
@@ -77,6 +116,7 @@ public class Seccion implements IContadorVoto {
     }
 
     /**
+     * Cuenta los votos totales.
      * @return cantidad de votos generales.
      */
     @Override
